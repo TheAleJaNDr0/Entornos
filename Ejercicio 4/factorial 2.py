842bab22a8e43cde6dc8e52b8205bea3f9529558
Alejandro Gómez <alejandrogomezmas@gmail.com>
2025-01-29 23:19:51 +0100

    Exercici 4

---
+++ Ejercicio 4/factorial 2.py
@@ -0,0 +1,20 @@
+def factioral(x):
+    """
+    Calculating the factorial of number using recursion
+
+    Parameters:
+        x: integer to calculate
+    Returns:
+        result: Factorial of number
+
+    """
+    if x == 1 or x == 0:
+        return 1
+    result = x * factioral(x - 1)
+    return result
+
+num = int(input("Enter a number to calculate it factorial: "))
+
+fact = factioral(num)
+
+print(f"The factorial of {num} is {fact}")
