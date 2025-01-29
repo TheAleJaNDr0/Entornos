b8c108cec2439de01431ff17a4037dd89c3e9fe6
Alejandro Gómez <alejandrogomezmas@gmail.com>
2025-01-29 23:13:47 +0100

    Exercici1.py

---
+++ Exercicis 2/Exercici1.py
@@ -0,0 +1,6 @@
+if __name__ == "__main__":
+    print("Introduce tu edad")
+    Edad = int(input())
+    print("Introduce tu peso")
+    Peso = int(input())
+    print(f"Tu edad es {Edad} y tu peso es {Peso}kg")
