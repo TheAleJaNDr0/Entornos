b1020e17dd374d4c4b6fd21d69aee60581e8dacd
Alejandro Gómez <alejandrogomezmas@gmail.com>
2025-01-29 23:14:14 +0100

    Exercici2.py

---
+++ Exercicis 2/Exercici2.py
@@ -0,0 +1,6 @@
+if __name__ == "__main__":
+    print("Introduce la base de un triangulo")
+    Base =int(input())
+    print("Introduce la altura del triangulo")
+    Altura = int(input())
+    print(f"El area de tu triangulo es {(Base*Altura)/2}")
